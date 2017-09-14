0ec43f9bccac6656e49a73b86d5ae37ab5ba18fb
Coen Stange <coen.stange@hotmail.com>
2017-09-14 18:18:02 +0200

    + add roll dice

--- CodClient/src/sample/GameController.java
+++ CodClient/src/sample/GameController.java
@@ -1,11 +1,40 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import soap.cod.CodSoap;
+import soap.cod.MyCodSoapService;
+
 public class GameController {
     private String username;
     private String password;
 
+    @FXML private Label labelName;
+    @FXML private Label labelRollResult;
+
     public void initialize(String user, String pass){
         username = user;
         password = pass;
+        labelName.setText(user);
+        showScoreBoard();
+    }
+
+    @FXML
+    private void rollDiceClicked(ActionEvent event) {
+        CodSoap server = new MyCodSoapService().getMyCodSoapPort();
+        boolean won = server.rollDice(username, password);
+        labelRollResult.setText(won ? "Won the dice roll :)" : "Lost the dice roll");
+        showScoreBoard();
+    }
+
+    @FXML
+    private void showScoreBoardClicked(ActionEvent event) {
+        showScoreBoard();
+    }
+
+    private void showScoreBoard() {
+
     }
 }
