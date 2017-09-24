d480e934c2195aeacb2d067fb572d12baeeb9797
Coen Stange <coen.stange@hotmail.com>
2017-09-24 21:39:10 +0200

    Revert "Revert "+ add cod client""
    This reverts commit 742ef0896eeb4595cca818b1bc80c8b6e6dbca51.

---
+++ REST/CodClient/src/sample/Controller.java
@@ -0,0 +1,68 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Controller {
+    private Main main;
+    /* Login */
+    @FXML private TextField nameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Text loginWarningText;
+
+    /* Register */
+    @FXML private TextField registerName;
+    @FXML private PasswordField registerPassword;
+    @FXML private PasswordField registerPasswordAgain;
+    @FXML private Text registerWarning;
+
+    @FXML
+    private void handleLoginClick(ActionEvent event) {
+        loginWarningText.setText("");
+        String name = nameField.getText();
+        String pass = passwordField.getText();
+        /*CodSoap server = new MyCodSoapService().getMyCodSoapPort();
+        StandardResult result = server.login(name, pass);
+
+        if (result.isSucces())
+        {
+            System.out.println("Login success");
+            main.loginSucceeded(name, pass);
+        }
+        else
+        {
+            loginWarningText.setText(result.getMessage());
+        }*/
+    }
+
+    @FXML
+    private void handleRegisterClick(ActionEvent event) {
+        registerWarning.setText("");
+        if (!registerPassword.getText().equals(registerPasswordAgain.getText()))
+        {
+            registerWarning.setText("Passwords are not the same");
+            return;
+        }
+        /*CodSoap server = new MyCodSoapService().getMyCodSoapPort();
+        StandardResult result = server.register(registerName.getText(), registerPassword.getText());
+
+        if (result.isSucces())
+        {
+            registerWarning.setText("Succesfully registered");
+            registerName.setText("");
+            registerPassword.setText("");
+            registerPasswordAgain.setText("");
+        }
+        else
+        {
+            registerWarning.setText(result.getMessage());
+        }*/
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
