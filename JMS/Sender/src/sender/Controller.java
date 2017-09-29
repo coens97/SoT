053f746c108ffb484bbcd9bf4e2a366fd1291e20
Coen Stange <coen.stange@hotmail.com>
2017-09-29 11:41:25 +0200

    + copy paste some code

--- JMS/Sender/src/sender/Controller.java
+++ JMS/Sender/src/sender/Controller.java
@@ -5,9 +5,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
 
 public class Controller {
     private Main main;
+    private Connection connection; // to connect to the ActiveMQ
+    private Session session; // session for creating messages, producers and
+    private Destination sendDestination; // reference to a queue/topic destination
+    private MessageProducer producer; // for sending messages
     /* Login */
     @FXML private TextField textInput;
     @FXML private ListView listView;
@@ -17,7 +27,28 @@ public class Controller {
 
     }
 
+    public Controller() {
+        try {
+            Properties props = new Properties();
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+            props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
 
+            // connect to the Destination called “myFirstChannel”
+            // queue or topic: “queue.myFirstDestination” or “topic.myFirstDestination”
+            props.put(("queue.myFirstDestination"), "myFirstDestination");
+            Context jndiContext = new InitialContext(props);
+            ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);    // connect to the sender destination
+            sendDestination = (Destination) jndiContext.lookup("myFirstDestination");
+            producer = session.createProducer(sendDestination);
+            String body = "Hello, this is my first message!"; //or serialize an object!
+            // create a text message          Message msg = session.createTextMessage(body);            // send the message             producer.send(msg);
+        } catch (NamingException | JMSException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public void setMain(Main main) {
         this.main = main;
